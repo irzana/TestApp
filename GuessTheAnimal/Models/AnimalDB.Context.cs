190682ad4ec10038476e045f49f8a99d7afcea73
KALANTHARF <KALANTHARF@PC11343>
2017-09-08 03:20:09 +0800

    Changes applied

--- GuessTheAnimal/Models/AnimalDB.Context.cs
+++ GuessTheAnimal/Models/AnimalDB.Context.cs
@@ -7,7 +7,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GuessTheAnimal
+namespace GuessTheAnimal.Models
 {
     using System;
     using System.Data.Entity;
